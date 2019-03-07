cac1532873e09cea8d64f723ef01be74ba029a0f
Marco Martinez <marco.mart1998@gmail.com>
2019-03-07 12:21:33 -0600

    Implemented methods into StackHospital except constructor

--- StackHospital.java
+++ StackHospital.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
 
-public class StackHospital {
+public class StackHospital<PatientType> extends Hospital<PatientType>
+{
+	public StackHospital()
+	{
+		ArrayList<PatientType> patientStack = new ArrayList<PatientType>();
+	}
+	
+	@Override
+	public void addPatient(PatientType patient) {
+		addPatient(patient);
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		return nextPatient();
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		return treatNextPatient();
+	}
+
+	@Override
+	public int numPatients() {
+		return numPatients();
+	}
+
+	@Override
+	public String hospitalType() {
+		return hospitalType();
+	}
+
+	@Override
+	public String allPatientInfo() {
+		return allPatientInfo();
+	}
+
 
 }
