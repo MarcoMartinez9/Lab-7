f1e28e013428ddcc3735915f623e0b6bafe52c9d
Marco Martinez <marco.mart1998@gmail.com>
2019-03-07 11:31:28 -0600

    Completed HealthyPerson and SickPerson class

--- HealthyPerson.java
+++ HealthyPerson.java
@@ -1,5 +1,23 @@
 
-public class HealthyPerson 
+public class HealthyPerson extends Person
 {
+	
+	private String reason;
+	
+	public HealthyPerson(String name, int age, String reason) {
+		super(name, age);
+		this.reason = reason;
+	}
 
+
+	@Override
+	protected int compareToImpl(Person p) 
+	{
+		return compareTo(p);
+	}
+	
+	public String toString()
+	{
+		return String.format("%s In for %s", getName(), reason);
+	}
 }
--- SickPerson.java
+++ SickPerson.java
@@ -1,5 +1,23 @@
 
-public class SickPerson 
+public class SickPerson extends Person
 {
+
+	private int severity;
+
+	public SickPerson(String name, int age, int severity) {
+		super(name, age);
+		this.severity = severity;
+	}
+
+	@Override
+	protected int compareToImpl(Person p) {
+		return compareTo(p);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("%s Severity level %d", getName(), severity);
+	}
 
 }
